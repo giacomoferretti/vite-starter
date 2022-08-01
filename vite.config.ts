7044f0cbce7f7e9d930e81cdb13deea07818544d
Giacomo Ferretti <giacomo.ferretti.00@gmail.com>
2022-08-01 19:35:40 +0200

    feat: add Github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vite-starter/",
   plugins: [react(), tsconfigPaths()],
 });
